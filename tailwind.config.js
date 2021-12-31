16352dac94ce3a3d6a08893b90655db3e556fca7
J. Eloy Chavez <laneloychavez@gmail.com>
2021-12-31 00:14:47 -0600

    v1.0
    Hice funcionar el Tailwind, agregue las primeras cards así como la cabecera del proyecto.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  content: [],
+  content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        'sanFrancisco': "url('https://www.viajarsanfrancisco.com/img/que-visitar-en-san-francisco.jpg')",
+        'noruega':"url('https://www.bbva.com/wp-content/uploads/2021/03/noruega.jpg')"
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9',
+      }),
+      textColor:{
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9',
+      },
+      fontFamily:{
+        Monserrat:['Monserrat','sans-serif']
+      },
+    },
   },
   plugins: [],
 }
+
