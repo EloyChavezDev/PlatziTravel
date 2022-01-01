47bc3dbc18450e56c04b6eece36b5bd406717807
J. Eloy Chavez <laneloychavez@gmail.com>
2021-12-31 21:41:00 -0600

    v1.1
    Proyecto Terminado, falta agregar los assets originales para no tener eso efecto indeseados.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   content: ["./public/index.html"],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage : {
         'sanFrancisco': "url('https://www.viajarsanfrancisco.com/img/que-visitar-en-san-francisco.jpg')",
-        'noruega':"url('https://www.bbva.com/wp-content/uploads/2021/03/noruega.jpg')"
+        'noruega':"url('https://www.bbva.com/wp-content/uploads/2021/03/noruega.jpg')",
+        'chicago':"url('https://i2.wp.com/mejoreszonas.com/wp-content/uploads/2018/12/Las-mejores-zonas-donde-alojarse-en-Chicago-Illinois.jpg')",
       },
       backgroundColor: theme => ({
         ...theme('colors'),
